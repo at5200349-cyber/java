ef57aabaad3877d4e0ec4706d110bd2e074de0d2
at5200349-cyber <at5200349@gmail.com>
2026-09-10 15:47:50 +0530

    Add Bustickenbokkingsystem class for bus ticket booking management with seat booking functionality

---
+++ Bustickenbokkingsystem.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+ class Bust {
+    int age;
+    
+    int availabelseat=50;
+
+    Bust(int a){
+        age=a;
+       
+    }
+    void Bookseat(int requiredseat) throws Exception{
+        if(age<=0){
+            throw new Exception("Invalid age");
+        }
+        if(requiredseat>availabelseat){
+            throw new Exception("Not available");
+        }
+        if(requiredseat<0){
+            throw new Exception("Invalid number ");
+        }
+        availabelseat-=requiredseat;
+        System.out.println("Seat booked successfully");
+    }
+   
+   
+}
+public class Bustickenbokkingsystem{
+   public static void main(String []arg){
+    Scanner sc=new Scanner(System.in);
+    try {
+        System.out.print("Enter age: ");
+        int age =sc.nextInt();
+        System.out.print("Enter seat to book: ");
+        int seats=sc.nextInt();
+
+
+        Bust passenger=new Bust(age);
+        passenger.Bookseat(seats);
+
+
+
+    }catch(java.util.InputMismatchException e){
+        System.out.println("Input a valid input ");
+    }
+     catch (Exception e) {
+        System.out.println(e.getMessage());
+    }
+   }
+
+
+    }
+
